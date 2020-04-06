299e76760a72531c099cb40cf6a01e48f1fcb1a7
Robson Santos <rtdcs.eng@gmail.com>
2020-04-06 19:37:15 -0300

    salvando desafio no git

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,113 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  let {title, techs, url} = request.body
+  techs = Array.from(techs)
+  const id = uuid();
+  const likes = 0
+
+  repositories.push(
+    {
+      id,
+      title,
+      techs,
+      url,
+      likes
+    }
+  );
+
+  console.log(repositories)
+
+  return response.json(
+    {
+      id,
+      title,
+      techs,
+      url,
+      likes
+    }
+  )
+
+
 });
 
 app.put("/repositories/:id", (request, response) => {
   // TODO
+  const {id} = request.params;
+  const index = repositories.findIndex(
+    (value)=>{
+      return value.id == id
+    }
+  );
+  
+  if (index < 0){
+    console.log(index)
+    return response.status(400).json({error: 'invalid id'})
+  }
+
+  const {title, url, techs} = request.body
+
+  console.log(title, url, techs)
+
+  const repository = repositories[index]
+
+  console.log(repository)
+
+  repository.title = title  
+  repository.url = url
+  repository.techs = techs
+
+  console.log(repository)
+
+  return response.json(repository)
+
 });
 
 app.delete("/repositories/:id", (req, res) => {
   // TODO
+  const {id} = req.params;
+  const index = repositories.findIndex(
+    (value)=>{
+      return value.id == id
+    }
+  );
+  
+  if (index < 0){
+    console.log(index)
+    return res.status(400).json({error: 'invalid id'})
+  }
+
+  let removed = repositories.splice(index,1)
+
+  return res.status(204).json(removed)
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
+
+  const {id} = request.params;
+  const repository = repositories.find(
+    (value)=>{
+      return value.id == id
+    }
+  );
+  
+  if (repository == undefined){
+    console.log(index)
+    return response.status(400).json({error: 'invalid id'})
+  }
+
+  repository.likes++ 
+
+  return response.json(repository)
+
+
+
 });
 
 module.exports = app;
